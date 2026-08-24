befb31eeedf2a11f28a8e0af5c1b5d2e1c3340b1
uniextra <2martinis@gmail.com>
2026-08-24 23:29:05 +0200

    chore: cleanup more temp scripts

--- update_html_favicon.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open("templates/index.html", "r", encoding="utf-8") as f:
-    html = f.read()
-
-html = html.replace("<title>Findly - Dashboard</title>", "<title>Findly - Dashboard</title>\n    <link rel=\"icon\" type=\"image/png\" href=\"/static/favicon.png\">")
-
-with open("templates/index.html", "w", encoding="utf-8") as f:
-    f.write(html)
